104beb045ee0da519908333439480616afd94d64
rianneogi <rianneogi@gmail.com>
2015-12-04 07:50:03 +0530

    Build 137

--- Blitzkrieg/main.cpp
+++ Blitzkrieg/main.cpp
@@ -25,11 +25,12 @@ using namespace std;
 
 string ENGINENAME = "Blitzkrieg";
 string ENGINEAUTHOR = "Rian Neogi";
-const int ENGINEVERSION = 136;
+const int ENGINEVERSION = 137;
 
 // Best Build so far: 129
 
 ///BUILDS
+// Build 137 - 03-12-2015 - Undid build 133
 // Build 136 - 03-12-2015 - Doubled pawn piecesq values and readded pawn duo bonuses
 // Build 135 - 03-12-2015 - Optimized so that we check whether a position is underCheck only once
 // Build 134 - 03-12-2015 - Removed i>=4 condition in LMR
