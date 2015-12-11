424111e011851f0882095c02d8d7953f4cd3a288
rianneogi <rianneogi@gmail.com>
2015-12-11 14:28:53 +0530

    Build 211

--- Blitzkrieg/main.cpp
+++ Blitzkrieg/main.cpp
@@ -23,11 +23,12 @@ using namespace std;
 
 string ENGINENAME = "Blitzkrieg";
 string ENGINEAUTHOR = "Rian Neogi";
-const int ENGINEVERSION = 210;
+const int ENGINEVERSION = 211;
 
 // Best Build so far: 191
 
 ///BUILDS
+// Build 211 - 11-12-2015 - Undid Pawn piecesq change in Build 209
 // Build 210 - 11-12-2015 - No longer factors in increment in time control
 // Build 209 - 11-12-2015 - Undid Build 207, Reworked Pawn Piecesq and reduced Pawn adj. for pieces a bit
 // Build 208 - 11-12-2015 - Added checkup() in Iterative Deepening
